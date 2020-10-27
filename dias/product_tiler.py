16e47795ffd209350608a76f007882825f61125f
Mihkel Veske <veskem@gmail.com>
2020-10-27 16:00:03 +0200

    Make ProductTiler logger

--- dias/product_tiler.py
+++ dias/product_tiler.py
@@ -2,23 +2,24 @@ import os
 from multiprocessing import Process, Queue
 
 import dias.utilities as utilities
+from dias.logger import Logger
 
 
-class ProductTiler:
-    def __init__(self, product_reader, indir):
+class ProductTiler(Logger):
+    def __init__(self, product_reader, data_dir):
+        super(ProductTiler, self).__init__(data_dir)
         self.n_threads = 10
         self.product_reader = product_reader
-        self.indir = indir
         self.cvat_exec = "../vsm/build/bin/cvat_vsm -d "
 
     def start(self):
         product_titles = self.product_reader.get_products()
         n_products = len(product_titles)
         if n_products == 0:
-            print('Nothing to split.')
+            self.info('Nothing to split.')
             return
 
-        print("Splitting {} products(s) with {} processes(s)".format(n_products, self.n_threads))
+        self.info("Splitting {} products(s) with {} processes(s)".format(n_products, self.n_threads))
 
         jobs_queue = Queue()
         [jobs_queue.put(product) for product in product_titles]
@@ -38,11 +39,11 @@ class ProductTiler:
         finally:
             [process.join() for process in processes]
 
-        print("Finished splitting.")
+        self.info("Finished splitting.")
 
     def split(self, control_queue, jobs_queue):
         # In case of ctrl+c, control_queue will become empty
         while (not control_queue.empty()) and (not jobs_queue.empty()):
             product_title = jobs_queue.get()
-            product_path = os.path.join(self.indir, product_title + ".SAFE")
+            product_path = os.path.join(self.data_dir, product_title + ".SAFE")
             utilities.execute(self.cvat_exec + product_path)
