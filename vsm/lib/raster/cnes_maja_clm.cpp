e220dc58294034be38f54012d05727243a245887
Indrek Sünter <indrek.sunter@kappazeta.ee>
2022-08-30 15:51:09 +0300

    Support for both MAJA flag formats

--- vsm/lib/raster/cnes_maja_clm.cpp
+++ vsm/lib/raster/cnes_maja_clm.cpp
@@ -21,11 +21,24 @@ CNES_MAJA_CLM_TIF::CNES_MAJA_CLM_TIF() {}
 CNES_MAJA_CLM_TIF::~CNES_MAJA_CLM_TIF() {}
 
 
-RasterImage *CNES_MAJA_CLM_TIF::remap_majac_values(RasterImage *img) {
+RasterImage *CNES_MAJA_CLM_TIF::remap_majac_values(RasterImage *img, clm_format_t flags_fmt) {
 	const float v_cloud = ESA_S2_SCL_JP2_Image::SCL_CLOUD_HIGH_PROBABILITY / 255.0f;
 	const float v_shadow = ESA_S2_SCL_JP2_Image::SCL_CLOUD_SHADOWS / 255.0f;
 	const float v_cirrus = ESA_S2_SCL_JP2_Image::SCL_THIN_CIRRUS / 255.0f;
 	const float v_clear = ESA_S2_SCL_JP2_Image::SCL_VEGETATION / 255.0f;
+	const float v_unsure = ESA_S2_SCL_JP2_Image::SCL_UNCLASSIFIED / 255.0f;
+	char f_cloud, f_shadow, f_cirrus;
+
+	// Pick flag combinations, depending on the format / classification scheme.
+	if (flags_fmt == CLM_FMT_THEIA) {
+		f_cloud = CLM_THEIA_CLOUDS;
+		f_shadow = CLM_THEIA_CLOUD_SHADOWS | CLM_THEIA_CLOUD_SHADOWS_OUTSIDE;
+		f_cirrus = CLM_THEIA_THIN_CLOUDS;
+	} else {
+		f_cloud = CLM_MAJA_CLOUDS;
+		f_shadow = CLM_MAJA_CLOUD_SHADOWS | CLM_MAJA_CLOUD_SHADOWS_OUTSIDE;
+		f_cirrus = CLM_MAJA_THIN_CLOUDS;
+	}
 
 	if (img != nullptr && img->subset != nullptr) {
 		unsigned int w = img->subset->columns();
@@ -41,14 +54,17 @@ RasterImage *CNES_MAJA_CLM_TIF::remap_majac_values(RasterImage *img) {
 
 			unsigned char value = (unsigned char) (255 * src_val);
 
-			if ((value & CLM_CLOUDS) != 0)
-				dst_val = v_cloud;
-			else if ((value & (CLM_CLOUD_SHADOWS | CLM_CLOUD_SHADOWS_OUTSIDE)) != 0)
-				dst_val = v_shadow;
-			else if ((value & CLM_THIN_CLOUDS) != 0)
-				dst_val = v_cirrus;
-			else
+			if (value == 0) {
 				dst_val = v_clear;
+			} else if ((value & f_cloud) != 0) {
+				dst_val = v_cloud;
+			} else if ((value & f_shadow) != 0) {
+				dst_val = v_shadow;
+			} else if ((value & f_cirrus) != 0) {
+				dst_val = v_cirrus;
+			} else {
+				dst_val = v_unsure;
+			}
 
 			px[i] = Magick::ColorGray(dst_val);
 		}
